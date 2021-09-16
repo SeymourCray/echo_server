62f97fd827d16ca68adda37b039cc5bd67f91f2c
SeymourCray <70951761+SeymourCray@users.noreply.github.com>
2021-09-16 19:50:20 +0300

    master

--- client.py
+++ client.py
@@ -6,12 +6,21 @@ HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))  # connect to server
-    while True:
-        # enter the text we want to send to server
-        text = str(input('write text: '))
-        if text == 'exit':
-            break
-        s.send(text.encode())           # encode text to bytes
-        data = s.recv(1024)             # get data in bytes from server
-        print(data.decode())            # decode and output data from server
+    try:
+        s.connect((HOST, PORT))  # connect to server
+        print('connection with server was successfull')
+    except:
+        print('Connection can not be complete')
+        exit()
+    try:
+        while True:
+            text = str(input('write text: ')) # enter the text we want to send to server
+            if text == 'exit':
+                break
+            s.send(text.encode())
+            print('we are sending data!')           # encode text to bytes
+            data = s.recv(1024)             # get data in bytes from server
+            print('we are received data!')
+            print(data.decode())            # decode and output data from server
+    except:
+        print('connection was lost')
--- server.py
+++ server.py
@@ -1,22 +1,34 @@
 #!/usr/bin/env python3
 
 import socket
+print('server is starting')
+
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))    # bind our socket to the given host and port
-    s.listen()
-    conn, addr = s.accept()  # new socket and client address
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            if data:
-                print('i got data!')
-            if not data:
-                break
-            data = data.decode().upper()  # change text
-            data = data.encode()
-            conn.send(data)
+    while True:
+        command = input()
+        if command == 'listen':
+            s.listen()
+        elif command == 'off':
+            break
+        print('port is listened')
+        conn, addr = s.accept()  # new socket and client address
+        print('connection was successfull')
+        with conn:
+            while True:
+                data = conn.recv(1024)
+                if data:
+                    print('i got data!')
+                else:
+                    print('client was disconnected')
+                    break
+                data = data.decode().upper()  # change text
+                data = data.encode()
+                conn.send(data)
+                print('data was sent')
+
+    print('server was turned off')

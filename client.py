6818d26b1da90ff8e8defa6c066364c27cc22506
SeymourCray <70951761+SeymourCray@users.noreply.github.com>
2021-09-16 13:42:39 +0300

    master

--- client.py
+++ client.py
@@ -6,11 +6,12 @@ HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
+    s.connect((HOST, PORT))  # connect to server
     while True:
+        # enter the text we want to send to server
         text = str(input('write text: '))
         if text == 'exit':
             break
-        s.send(text.encode())
-        data = s.recv(1024)
-        print(data.decode())
+        s.send(text.encode())           # encode text to bytes
+        data = s.recv(1024)             # get data in bytes from server
+        print(data.decode())            # decode and output data from server
--- server.py
+++ server.py
@@ -6,9 +6,9 @@ HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
+    s.bind((HOST, PORT))    # bind our socket to the given host and port
     s.listen()
-    conn, addr = s.accept()
+    conn, addr = s.accept()  # new socket and client address
     with conn:
         print('Connected by', addr)
         while True:
@@ -17,4 +17,6 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 print('i got data!')
             if not data:
                 break
+            data = data.decode().upper()  # change text
+            data = data.encode()
             conn.send(data)
